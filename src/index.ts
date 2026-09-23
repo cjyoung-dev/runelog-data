45650efda4f47d5b287a297d05b8660c4f66cb14
Carson Young <carson.young18@gmail.com>
2026-09-23 16:20:54 -0500

    fixed player table query

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,25 @@ const debug = logger('index');
 export default {
 	async scheduled(event, env, ctx): Promise<void> {
 		const db = await getDb(env.HYPERDRIVE);
-		const players = await db.select().from(playersTable);
+
+		let players;
+		try {
+			players = await db.select().from(playersTable);
+		}
+		catch (err) {
+			debug('Error while running player query', err);
+			return;
+		}
 
 		for (const p of players) {
-			try
-			{
+			try {
 				debug(p);
 				const data = await fetchPlayerData(p.name);
 				debug(data);
 			}
-			catch(err)
-			{
-				debug(err);
+			catch(err) {
+				debug(`Error while fetching ${p.name}`, err);
 			}
 		}
-	},
+	}
 } satisfies ExportedHandler<Env>
